030ac9e2d0f6b0e63e94148bac2fa248db0bc94c
kushkota <kssapko@LAPTOP-3C2309SK.localdomain>
2022-05-31 16:05:13 -0400

    Getting Started with Java 17 Training

---
+++ GrossPayCalculator.java
@@ -0,0 +1,27 @@
+
+// Package: Folder or container that holds Java files
+// package option serves as a folder to organize Java files
+// package declaration
+
+package gross_calculator;
+
+// package contains java files, also known as classes
+// class option represents a Java file
+// class declaration
+
+public class GrossPayCalculator {
+
+// the curly braces are used to encompass a block of code
+
+	
+	// main method
+	// the entry point for running a program in Java
+	// When we execute this class, it's going to look for the main method within this class, and
+	// this is the starting point of the program
+	public static void main(String[] args) {
+
+		System.out.println("Hello World!");
+
+		
+	}
+}
